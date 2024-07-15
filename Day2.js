905586cd916f19e45cbaf5df08f804b24ff804de
Rahul Yadav <rahul3586yadav@gmail.com>
2024-07-15 11:43:28 +0530

    Day 2

---
+++ Day2.js
@@ -0,0 +1,35 @@
+//Activity 1: Arithmetic Operator
+let num1 = 40;
+let num2 = 12;
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1%num2);
+
+//Activity 2: Assignment Operator
+num1 += 10;
+num2 -= 10
+console.log(num1);
+console.log(num2);
+
+//Activity 3: Comparision Operator
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+console.log(num1 == num2);
+console.log(num1 === num2);
+
+//Activity 4: Logical Operator
+console.log(num1 > num2 && num1 >= num2);
+console.log(num1 > num2 && num1 <= num2);
+console.log(num1 > num2 || num1 == num2);
+console.log(num1 != num2);
+
+//Activity 5: Ternary Operator
+con = num1 > 0 ? 'Positive' : 'Negative' ;
+console.log(con);
+num3 = -20;
+con2 = num3 > 0 ? 'Positive' : 'Negative' ;
+console.log(con2);
