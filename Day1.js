16a51648cf3322926ab74450906dc6d93c8c94de
Rahul Yadav <rahul3586yadav@gmail.com>
2024-07-15 11:20:00 +0530

    First Commit

---
+++ Day1.js
@@ -0,0 +1,33 @@
+//Activity 1:
+var num = 10;
+console.log(num);
+
+let name = "Rahul";
+console.log(name);
+
+//Activity 2:
+const type = true;
+console.log(type);
+
+//Activity 3:
+var obj = {name:'Rahul', rollNo: 42};
+var arr = [1,2,3,4];
+console.log(typeof(num));
+console.log(typeof(name));
+console.log(typeof(type));
+console.log(typeof(obj));
+console.log(typeof(arr));
+
+//Activity 4:
+let num2 = 45;
+console.log(num2);
+num2 = 54;
+console.log(num2);
+
+//Activity 5:
+const numb = 45;
+console.log(numb);
+numb = 54;
+console.log(numb)
+
+//Feature Request :
